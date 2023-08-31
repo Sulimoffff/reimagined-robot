8ac6e6c757f3848831d875f83b1e1c87bf12c44e
Sulimoffff <sylimov395@gmail.com>
2023-08-31 12:47:18 +0300

    Обновлены файлы

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-function addNotebook() {
-  var model = document.getElementById("model").value;
-  var serialNumber = document.getElementById("serialNumber").value;
-  var issuedTo = document.getElementById("issuedTo").value;
-  
-  var notebookItem = document.createElement("li");
-  notebookItem.textContent = "Модель: " + model + ", Серийный номер: " + serialNumber + ", Выдан: " + issuedTo;
-  
-  var notebookList = document.getElementById("notebookList");
-  notebookList.appendChild(notebookItem);
-  
-  // Очистка полей после добавления
-  document.getElementById("model").value = "";
-  document.getElementById("serialNumber").value = "";
-  document.getElementById("issuedTo").value = "";
+const notebookTable = document.getElementById('notebook-table').getElementsByTagName('tbody')[0];
+
+// Добавляем данные в таблицу
+function addRow(name, date, specs) {
+    const newRow = notebookTable.insertRow();
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    const cell3 = newRow.insertCell(2);
+
+    cell1.innerHTML = name;
+    cell2.innerHTML = date;
+    cell3.innerHTML = specs;
 }
+
+// Вызываем функцию для добавления данных
+addRow('Иванов Иван', '01.09.2023', 'Модель: HP EliteBook, RAM: 8GB, HDD: 256GB SSD');
+addRow('Петрова Елена', '15.09.2023', 'Модель: Dell Latitude, RAM: 16GB, SSD: 512GB');
+// Добавьте дополнительные строки для других сотрудников
